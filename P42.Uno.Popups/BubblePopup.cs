22b1e3251bfc1da313d6c06b76d2b075d87675c5
Ben Askren <ben@buildcalc.com>
2020-09-30 02:48:47 -0400

    well, it was working an hour ago,

--- P42.Uno.Popups/BubblePopup.cs
+++ P42.Uno.Popups/BubblePopup.cs
@@ -3,17 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -238,19 +232,10 @@ namespace P42.Uno.Popups
 
 
         #region Construction / Initialization
-        static BubblePopup()
-        {
-
-        }
 
         public BubblePopup()
         {
-            DefaultStyleKey = typeof(BubblePopup);
-
-            var xstyle = Application.Current.Resources["BubblePopupStyle"] as Style;
-            Style = xstyle;
-            bool rebuilt = ApplyTemplate();
-
+            this.DefaultStyleKey = typeof(BubblePopup);
         }
 
 
