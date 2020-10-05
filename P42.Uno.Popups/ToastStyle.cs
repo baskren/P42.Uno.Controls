a38ce240c5c33b5dd5d9ec41e6aedbc3b17eea84
Ben Askren <ben@buildcalc.com>
2020-10-05 15:18:22 -0400

    using Style to create Toast class!

---
+++ P42.Uno.Popups/ToastStyle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace P42.Uno.Popups
+{
+    public partial class ToastStyle : TargetedPopup
+    {
+        public ToastStyle()
+        {
+            var style = Application.Current.Resources["TargetedToastPopupTemplate"] as Style;
+            Style = style;
+        }
+    }
+}
