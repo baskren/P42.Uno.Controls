695a4b151a4956458adee4996818d3320b8988f9
Ben Askren <ben@buildcalc.com>
2020-10-14 21:32:30 -0400

    Added Settings.Init() and Settings.AssembliesToInclude;

---
+++ P42.Uno.Controls/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P42.Uno.Controls
+{
+    public static class Settings
+    {
+        public static List<Assembly> AssembliesToInclude => P42.Utils.Uno.Settings.AssembliesToInclude;
+
+        public static void Init(Windows.UI.Xaml.Application application)
+        {
+            P42.Utils.Uno.Settings.Init(application);
+            AssembliesToInclude.Add(typeof(Settings).Assembly);
+        }
+    }
+}
