d0a6ca99996254789d96c1858563c8ffcfdfb9e6
Ben Askren <ben@buildcalc.com>
2020-11-04 17:59:25 -0500

    Fixed: AlignedPlacementPanel overflow

--- P42.Uno.Controls/AlignedPlacementPanel.cs
+++ P42.Uno.Controls/AlignedPlacementPanel.cs
@@ -1,10 +1,12 @@
-﻿using P42.Utils.Uno;
+﻿using P42.Uno.Markup;
+using P42.Utils.Uno;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
@@ -16,8 +18,55 @@ namespace P42.Uno.Controls
         Button _moreButton = new Button
         {
             Content = "...",
+            Background = Colors.Transparent.ToBrush()
         };
-        List<FrameworkElement> MoreChildren = new List<FrameworkElement>();
+        List<UIElement> MoreChildren = new List<UIElement>();
+
+        TargetedPopup _popup;
+
+        public AlignedPlacementPanel()
+        {
+            Children.Add(_moreButton);
+            _moreButton.Click += OnMoreButtonClicked;
+        }
+
+        async void OnMoreButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var stack = new StackPanel()
+                .Spacing(3)
+                //.Margin(0)
+                //.Padding(0)
+                .Children(
+                    MoreChildren
+                );
+            var w = stack.Margin.Horizontal() + stack.Padding.Horizontal();
+            var h = stack.Margin.Vertical() + stack.Padding.Vertical();
+            foreach (var child in MoreChildren)
+            {
+                if (child is Button b)
+                    b.Click += OnMoreButtonChildClicked;
+                w = Math.Max(w, child.DesiredSize.Width);
+                h += child.DesiredSize.Height + 3;
+            }
+            stack.Width = w;
+            stack.Height = h;
+            _popup = await P42.Uno.Controls.TargetedPopup.CreateAsync(_moreButton, stack);
+            _popup.Padding(2);
+            await _popup.WaitForPoppedAsync();
+            foreach (var child in MoreChildren)
+            {
+                if (child is Button b)
+                    b.Click -= OnMoreButtonChildClicked;
+                stack.Children.Remove(child);
+            }
+            _popup = null;
+        }
+
+        async void OnMoreButtonChildClicked(object sender, RoutedEventArgs e)
+        {
+            if (_popup is TargetedPopup p)
+                await p.PopAsync();
+        }
 
         protected override Size MeasureOverride(Size availableSize)
         {
@@ -32,15 +81,19 @@ namespace P42.Uno.Controls
 
         protected override Size ArrangeOverride(Size finalSize)
         {
+            foreach (var child in MoreChildren)
+                Children.Add(child);
             MoreChildren.Clear();
 
-            List<FrameworkElement> leftChildren = new List<FrameworkElement>();
-            List<FrameworkElement> centerChildren = new List<FrameworkElement>();
-            List<FrameworkElement> rightChildren = new List<FrameworkElement>();
+            var leftChildren = new List<FrameworkElement>();
+            var centerChildren = new List<FrameworkElement>();
+            var rightChildren = new List<FrameworkElement>();
 
             var width = 0.0;
             foreach (var child in Children)
             {
+                if (child == _moreButton)
+                    continue;
                 if (child is FrameworkElement element)
                 {
                     if (element.HorizontalAlignment == HorizontalAlignment.Left)
@@ -55,11 +108,13 @@ namespace P42.Uno.Controls
                     throw new Exception("only FrameworkElement is allowed to be a child of " + GetType() + ".  ["+child+"] is not a FrameworkElement.");
             }
 
-            if (width > finalSize.Width)
-                centerChildren.Insert(0, _moreButton);
             var leftWidth = 0.0;
             var rightWidth = 0.0;
             var centerWidth = 0.0;
+            if (width > finalSize.Width)
+            {
+                centerChildren.Insert(0, _moreButton);
+            }
             var maxSectionCount = Math.Max(leftChildren.Count, Math.Max(rightChildren.Count, centerChildren.Count));
             for (int i=0;i< maxSectionCount;i++)
             {
@@ -115,7 +170,10 @@ namespace P42.Uno.Controls
                 centerX += element.DesiredSize.Width;
             }
 
-            return base.ArrangeOverride(finalSize);
+            foreach (var child in MoreChildren)
+                Children.Remove(child);
+
+            return finalSize;
         }
     }
 }
