5f9b9b7e1998a5e52e38ecb9b428c7202a4ad2f9
Ben Askren <ben@buildcalc.com>
2023-02-16 07:36:41 -0500

    INTERMEDIATE

---
+++ P42.Uno.Controls/Popups/PopupBase.shared.cs
@@ -0,0 +1,39 @@
+using P42.Uno.Markup;
+using P42.Utils.Uno;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Shapes;
+
+
+namespace P42.Uno.Controls
+{
+    partial class PopupBase : UserControl
+    {
+        #region Properties
+
+        #endregion
+
+
+        #region Events
+        #endregion
+
+
+        #region Construction
+
+        #endregion
+
+
+
+    }
+}
