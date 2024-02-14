89368897498b80afab36e58c6405fe9cc5136eea
baskren <ben@buildcalc.com>
2024-02-14 09:13:25 -0500

    REFACTOR : EmbeddedSvgImage code cleanup

--- P42.Uno.Controls/EmbeddedSvgImage/EmbeddedSvgImage.shared.cs
+++ P42.Uno.Controls/EmbeddedSvgImage/EmbeddedSvgImage.shared.cs
@@ -1,4 +1,4 @@
-﻿using P42.Utils.Uno;
+using P42.Utils.Uno;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
@@ -100,102 +100,56 @@ namespace P42.Uno.Controls
 
         private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
-            var canvas = e.Surface?.Canvas;
-            if (canvas == null)
+            if (e?.Surface?.Canvas is not SKCanvas workingCanvas)
                 return;
 
-            var clipBounds = canvas.LocalClipBounds;
-
-            var workingCanvas = canvas;
-            var fillRect = e.Info.Rect;
-
             workingCanvas.Clear();
-            if (_skSvg?.Picture is null)
+            if (_skSvg?.Picture is not SKPicture picture)
                 return;
             workingCanvas.Save();
 
+            var fillRect = e.Info.Rect;
             var fillRectAspect = (float)fillRect.Width / (float)fillRect.Height;
-            var imageAspect = (_skSvg.CanvasSize.Width < 1 || _skSvg.CanvasSize.Height < 1) ? 1 : _skSvg.CanvasSize.Width / _skSvg.CanvasSize.Height;
-            double scaleX = 1.0;
-            double scaleY = 1.0;
+            var imageAspect = (_skSvg.CanvasSize.Width < 1 || _skSvg.CanvasSize.Height < 1) 
+                ? 1 
+                : _skSvg.CanvasSize.Width / _skSvg.CanvasSize.Height;
+
             if (_skSvg.CanvasSize.Width <= 0 || _skSvg.CanvasSize.Height <= 0)
             {
-                scaleX = scaleY = 1;
                 Console.WriteLine("Cannot tile, scale or justify an SVG image with zero or negative Width or Height. Verify, in the SVG source, that the x, y, width, height, and viewBox attributes of the <SVG> tag are present and set correctly.");
             }
             else if (Stretch == Stretch.UniformToFill)
             {
-                scaleX = imageAspect > fillRectAspect ? fillRect.Height / _skSvg.CanvasSize.Height : fillRect.Width / _skSvg.CanvasSize.Width;
-                scaleY = scaleX;
+                var scale = imageAspect > fillRectAspect 
+                    ? fillRect.Height / _skSvg.CanvasSize.Height 
+                    : fillRect.Width / _skSvg.CanvasSize.Width;
+                workingCanvas.Scale(scale, scale);
             }
             else if (Stretch == Stretch.Uniform)
             {
-                scaleX = imageAspect > fillRectAspect ? fillRect.Width / _skSvg.CanvasSize.Width : fillRect.Height / _skSvg.CanvasSize.Height;
-                scaleY = scaleX;
+                var scale = imageAspect > fillRectAspect 
+                    ? fillRect.Width / _skSvg.CanvasSize.Width 
+                    : fillRect.Height / _skSvg.CanvasSize.Height;
+                workingCanvas.Scale(scale, scale);
             }
             else if (Stretch == Stretch.Fill)
             {
-                scaleX = fillRect.Width / _skSvg.CanvasSize.Width;
-                scaleY = fillRect.Height / _skSvg.CanvasSize.Height;
+                var scaleX = fillRect.Width / _skSvg.CanvasSize.Width;
+                var scaleY = fillRect.Height / _skSvg.CanvasSize.Height;
+                workingCanvas.Scale(scaleX, scaleY);
             }
 
-            var scaledWidth = _skSvg.CanvasSize.Width * scaleX;
-            var scaledHeight = _skSvg.CanvasSize.Height * scaleY;
 
-            float left = 0;
-            float top = 0;
-            if (_skSvg.CanvasSize.Width > 0 && _skSvg.CanvasSize.Height > 0)
+            if (Opacity < 1.0 && Opacity >= 0)
             {
-                switch (HorizontalAlignment)
-                {
-                    case HorizontalAlignment.Left:
-                        left = 0;
-                        break;
-                    case HorizontalAlignment.Right:
-                        left = (float)(fillRect.Width - scaledWidth);
-                        break;
-                    default:
-                        left = (float)(fillRect.Width - scaledWidth) / 2.0f;
-                        break;
-                }
-                switch (VerticalAlignment)
-                {
-                    case VerticalAlignment.Top:
-                        top = 0;
-                        break;
-                    case VerticalAlignment.Bottom:
-                        top = (float)(fillRect.Height - scaledHeight);
-                        break;
-                    default:
-                        top = (float)(fillRect.Height - scaledHeight) / 2.0f;
-                        break;
-                }
+                var alpha = (byte)(Opacity * 255);
+                var transparency = SKColors.White.WithAlpha(alpha); 
+                var paint = new SKPaint { ColorFilter = SKColorFilter.CreateBlendMode(transparency, SKBlendMode.DstIn) };
+                workingCanvas.DrawPicture(picture, paint);
             }
-            //var shadowPadding = ShapeBase.ShadowPadding(this, true);
-            //workingCanvas.Translate(left + (float)shadowPadding.Left, top + (float)shadowPadding.Top);
-            workingCanvas.Scale((float)scaleX, (float)scaleY);
-            SKPaint paint = null;
-            /*
-            if (shadowPaint == null && TintColor != Color.Default && TintColor != Color.Transparent)
-            {
-                var color = new SKColor(TintColor.ByteR(), TintColor.ByteG(), TintColor.ByteB(), TintColor.ByteA());
-                var cf = SKColorFilter.CreateBlendMode(color, SKBlendMode.SrcIn);
+            else
+                workingCanvas.DrawPicture(picture);
 
-                paint = new SKPaint
-                {
-                    ColorFilter = cf,
-                    IsAntialias = true
-                };
-            }
-            else 
-            */
-            if (Opacity < 1.0)
-            {
-                var transparency = SKColors.White.WithAlpha((byte)(Opacity * 255)); // 127 => 50%
-                paint = new SKPaint { ColorFilter = SKColorFilter.CreateBlendMode(transparency, SKBlendMode.DstIn) };
-            }
-            //workingCanvas.Clear();
-            workingCanvas.DrawPicture(_skSvg.Picture, paint);
             workingCanvas.Restore();
 
 
